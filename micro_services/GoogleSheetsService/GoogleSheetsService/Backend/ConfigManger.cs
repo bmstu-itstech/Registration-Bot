672da5bfd2c15763ce5e9bde7e897c594a1080b9
Encapsulateed <alexey.mitroshkin@gmail.com>
2023-03-14 19:13:14 +0300

    litle bugfix

---
+++ micro_services/GoogleSheetsService/GoogleSheetsService/Backend/ConfigManger.cs
@@ -0,0 +1,32 @@
+﻿namespace GoogleSheetsService.Backend
+{
+    public class ConfigManager
+    {
+        private readonly string appsettings_path = "appsettings.json";
+
+        private ConfigurationBuilder _configurationBuilder;
+        private IConfigurationRoot _config;
+        private bool DEBUG = false;
+
+        public ConfigManager()
+        {
+            _configurationBuilder = new ConfigurationBuilder();
+            _configurationBuilder.AddJsonFile(appsettings_path);
+            _config = _configurationBuilder.Build();
+
+            string debug = _config.GetValue<string>("DEBUG");
+
+            if (debug == "true")
+                DEBUG = true;
+
+        }
+
+        public string GetConnetion()
+        {
+            if (DEBUG)
+                return _config.GetConnectionString("DEBAG_DB");
+            return _config.GetConnectionString("DB");
+        }
+
+    }
+}
