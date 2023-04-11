2652e4f251dbb162954c3dbbd08340c90ab1f06b
darleet <darleet@mail.ru>
2023-04-11 21:33:11 +0300

    Small changes

--- asker_bot/bot_instance.py
+++ asker_bot/bot_instance.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import asyncpg
 
@@ -14,6 +15,76 @@ from redis import asyncio as aioredis
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
+
+# Database connect function
+async def connect_or_create(user, database) -> asyncpg.Connection:
+    try:
+        conn = await asyncpg.connect(user=user, database=database)
+    except asyncpg.InvalidCatalogNameError:
+        # Database does not exist, create it.
+        sys_conn = await asyncpg.connect(
+            database='template1',
+            user='postgres'
+        )
+        await sys_conn.execute(
+            f'CREATE DATABASE "{database}" OWNER "{user}"',
+        )
+        await sys_conn.close()
+
+        # Connect to the newly created database.
+        conn = await asyncpg.connect(user=user, database=database)
+
+    return conn
+
+
+async def get_question_text(conn, question_id):
+    # Get question text from database
+    question_text = await conn.fetchrow('SELECT question_text FROM questions WHERE id = ?',
+                                        (question_id,))
+    return question_text[0]
+
+async def get_question_type(conn, question_id):
+    # Get question type from database
+    question_type = await conn.fetchrow('SELECT type FROM questions WHERE id = ?',
+                                        (question_id,))
+    return question_type[0]
+
+async def get_question_options(conn, question_id):
+    # Get question options from database
+    question_options = await conn.fetch('SELECT (id, answer_text) FROM buttons '
+                                        'WHERE question_id = ?',
+                                        (question_id,))
+    return question_options[0] if question_options is not None else None
+
+async def get_next_question_id(conn, button_id):
+    # Get next question ID based on button ID
+    next_question_id = await conn.fetchrow('SELECT next_question_id FROM buttons '
+                                           'WHERE id = ?',
+                                           (button_id,))
+    return next_question_id[0]
+
+async def send_question(bot_id, state, bot, chat_id, question_id):
+    async with asyncpg.connect(f'postgresql://darleet@localhost/id{bot_id}') as conn:
+        # Get question text and type from database
+        question_text = await get_question_text(conn, question_id)
+        question_type = await get_question_type(conn, question_id)
+
+    # Send question
+    if question_type == 'text':
+        await bot.send_message(chat_id, question_text)
+    elif question_type == 'buttons':
+        # Get question options from database (if applicable)
+        question_options = await get_question_options(conn, question_id)
+
+        keyboard = InlineKeyboardMarkup(row_width=1)
+
+        for option in question_options:
+            button = InlineKeyboardButton(text=option[1], callback_data=option[0])
+            keyboard.construct(button)
+
+        await bot.send_message(chat_id, question_text, reply_markup=keyboard)
+
+    await state.update_data(question_id=question_id)
 
 # Set up startup handler
 async def run_instance(token, bot_id):
@@ -35,100 +106,51 @@ async def run_instance(token, bot_id):
     dp.include_router(router)
 
     # Create database connection
-    async with asyncpg.connect(f'id{bot_id}.db') as conn:
+    conn = await connect_or_create('postgresql', f'id{bot_id}')
 
-        # Create questions table
-        await conn.execute('''
-                CREATE TABLE IF NOT EXISTS questions (
-                    id INTEGER PRIMARY KEY,
-                    question_text TEXT NOT NULL,
-                    type TEXT NOT NULL DEFAULT "text"
-                )
-            ''')
+    # Create questions table
+    await conn.execute('''
+            CREATE TABLE IF NOT EXISTS questions (
+                id INTEGER PRIMARY KEY,
+                question_text TEXT NOT NULL,
+                type TEXT NOT NULL DEFAULT "text"
+            )
+        ''')
 
-        # Create buttons table
-        await conn.execute('''
-                CREATE TABLE IF NOT EXISTS buttons (
-                    id INTEGER PRIMARY KEY,
-                    question_id INTEGER NOT NULL,
-                    answer_text TEXT NOT NULL,
-                    next_question_id INTEGER,
-                    FOREIGN KEY (question_id) REFERENCES questions(id)
-                )
-            ''')
+    # Create buttons table
+    await conn.execute('''
+            CREATE TABLE IF NOT EXISTS buttons (
+                id INTEGER PRIMARY KEY,
+                question_id INTEGER NOT NULL,
+                answer_text TEXT NOT NULL,
+                next_question_id INTEGER,
+                FOREIGN KEY (question_id) REFERENCES questions(id)
+            )
+        ''')
 
-        # Insert sample data
-        await conn.execute('''
-                INSERT INTO questions (question_text, type)
-                VALUES
-                    ("Как тебя зовут?", "text"),
-                    ("Ты совершеннолетний?", "text"),
-                    ("Какой твой любимый цвет?", "buttons"),
-                    ("То есть ты фанат красного?", "buttons"),
-                    ("То есть ты фанат синего?", "buttons")
-            ''')
+    # Insert sample data
+    await conn.execute('''
+            INSERT INTO questions (question_text, type)
+            VALUES
+                ("Как тебя зовут?", "text"),
+                ("Ты совершеннолетний?", "text"),
+                ("Какой твой любимый цвет?", "buttons"),
+                ("То есть ты фанат красного?", "buttons"),
+                ("То есть ты фанат синего?", "buttons")
+        ''')
 
-        await conn.execute('''
-                INSERT INTO buttons (question_id, answer_text, next_question_id)
-                VALUES
-                    (3, "Красный", 4),
-                    (3, "Синий", 5),
-                    (4, "Да", NULL),
-                    (4, "Нет", NULL),
-                    (5, "Да", NULL),
-                    (5, "Нет", NULL)
-            ''')
+    await conn.execute('''
+            INSERT INTO buttons (question_id, answer_text, next_question_id)
+            VALUES
+                (3, "Красный", 4),
+                (3, "Синий", 5),
+                (4, "Да", NULL),
+                (4, "Нет", NULL),
+                (5, "Да", NULL),
+                (5, "Нет", NULL)
+        ''')
 
-        await conn.commit()
-
-    async def get_question_text(conn, question_id):
-        # Get question text from database
-        question_text = await conn.fetchrow('SELECT question_text FROM questions WHERE id = ?',
-                                            (question_id,))
-        return question_text[0]
-
-    async def get_question_type(conn, question_id):
-        # Get question type from database
-        question_type = await conn.fetchrow('SELECT type FROM questions WHERE id = ?',
-                                            (question_id,))
-        return question_type[0]
-
-    async def get_question_options(conn, question_id):
-        # Get question options from database
-        question_options = await conn.fetch('SELECT (id, answer_text) FROM buttons '
-                                              'WHERE question_id = ?',
-                                            (question_id,))
-        return question_options[0] if question_options is not None else None
-
-    async def get_next_question_id(conn, button_id):
-        # Get next question ID based on button ID
-        next_question_id = await conn.fetchrow('SELECT next_question_id FROM buttons '
-                                                 'WHERE id = ?',
-                                               (button_id,))
-        return next_question_id[0]
-
-    async def send_question(state, bot, chat_id, question_id):
-        async with asyncpg.connect(f'id{bot_id}.db') as conn:
-            # Get question text and type from database
-            question_text = await get_question_text(conn, question_id)
-            question_type = await get_question_type(conn, question_id)
-
-        # Send question
-        if question_type == 'text':
-            await bot.send_message(chat_id, question_text)
-        elif question_type == 'buttons':
-            # Get question options from database (if applicable)
-            question_options = await get_question_options(conn, question_id)
-
-            keyboard = InlineKeyboardMarkup(row_width=1)
-
-            for option in question_options:
-                button = InlineKeyboardButton(text=option[1], callback_data=option[0])
-                keyboard.construct(button)
-
-            await bot.send_message(chat_id, question_text, reply_markup=keyboard)
-
-        await state.update_data(question_id=question_id)
+    await conn.close()
 
     @router.message()
     async def answer_handler(message: types.Message, state: FSMContext):
@@ -138,12 +160,12 @@ async def run_instance(token, bot_id):
         next_question_id = question_id + 1
 
         # Send next question
-        await send_question(state, bot, message.chat.id, next_question_id)
+        await send_question(bot_id, state, bot, message.chat.id, next_question_id)
 
     # Start command
     @router.message(CommandStart())
     async def cmd_start(message: types.Message, state: FSMContext):
-        await send_question(state, bot, message.chat.id, 1)
+        await send_question(bot_id, state, bot, message.chat.id, 1)
 
     # Callback query handler
     @router.callback_query()
@@ -152,14 +174,11 @@ async def run_instance(token, bot_id):
         button_id = callback_query.data
 
         # Send next question based on button ID
-        await send_question(state, bot, callback_query.message.chat.id, button_id)
+        await send_question(bot_id, state, bot, callback_query.message.chat.id, button_id)
 
-    # # Default message
-    # @router.message()
-    # async def default_handler(message: types.Message):
-    #     await bot.send_message(message.chat.id, 'Неверный формат сообщения!')
+    await dp.start_polling(bot)
 
-    async def main():
-        await dp.start_polling(bot)
 
-    await main()
+if __name__ == '__main__':
+    import config
+    asyncio.run(run_instance(config.bot_token, 1))
--- constructor/writer.py
+++ constructor/writer.py
@@ -1,6 +1,26 @@
 from constructor.tree import Tree
 import asyncpg
 
+# Database connect function
+async def connect_or_create(user, database) -> asyncpg.Connection:
+    try:
+        conn = await asyncpg.connect(user=user, database=database)
+    except asyncpg.InvalidCatalogNameError:
+        # Database does not exist, create it.
+        sys_conn = await asyncpg.connect(
+            database='template1',
+            user='postgres'
+        )
+        await sys_conn.execute(
+            f'CREATE DATABASE "{database}" OWNER "{user}"',
+        )
+        await sys_conn.close()
+
+        # Connect to the newly created database.
+        conn = await asyncpg.connect(user=user, database=database)
+
+    return conn
+
 # Эта функция будет заполнять данными дерево и связываться с микросервисом БД
 async def create_tree(data, bot_id):
     tree_ent = Tree(bot_id)
@@ -8,28 +28,26 @@ async def create_tree(data, bot_id):
     for key in data.keys():
         tree_ent.add_module(key, data[key]["links"], data[key]["question"], data[key]["answers"])
 
-    # Написал максимально простой тест дерева на асинхронном SQLite
-    async with asyncpg.connect(f'./id{bot_id}.db') as conn:
+    # Create database connection
+    conn = await connect_or_create('postgresql', f'id{bot_id}')
 
+    sql = (
+        f"DROP TABLE IF EXISTS id{bot_id}"
+    )
+    await conn.execute(sql)
+
+    sql = (
+        f"CREATE TABLE id{bot_id} ("
+        f"module_id INTEGER PRIMARY KEY,"
+        f"next_ids text,"
+        f"question_text text NOT NULL,"
+        f"answers text)"
+    )
+    await conn.execute(sql)
+
+    for module in tree_ent.get_data():
         sql = (
-            f"DROP TABLE IF EXISTS id{bot_id}"
+            f"INSERT INTO id{bot_id} (module_id, next_ids, question_text, answers) "
+            f"VALUES (?, ?, ?, ?)"
         )
-        await conn.execute(sql)
-
-        sql = (
-            f"CREATE TABLE id{bot_id} ("
-            f"module_id INTEGER PRIMARY KEY,"
-            f"next_ids text,"
-            f"question_text text NOT NULL,"
-            f"answers text)"
-        )
-        await conn.execute(sql)
-
-        for module in tree_ent.get_data():
-            sql = (
-                f"INSERT INTO id{bot_id} (module_id, next_ids, question_text, answers) "
-                f"VALUES (?, ?, ?, ?)"
-            )
-            await conn.execute(sql, module)
-
-        await conn.execute("COMMIT;")
+        await conn.execute(sql, module)
