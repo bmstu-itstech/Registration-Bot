14d5d8c2e0e1788e0412cb6d026be2d9c3a8bb08
Matvey <68477540+MathewPr@users.noreply.github.com>
2023-09-05 15:50:48 +0300

    ITSR-21. Carousel finished. checkDeadEnd function added.

--- Frontend/src/app/account/list-page/list-page.component.ts
+++ Frontend/src/app/account/list-page/list-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, AfterViewInit, Renderer2} from '@angular/core';
 import {CarouselComponent} from "ngx-bootstrap/carousel";
 
 @Component({
@@ -30,16 +30,51 @@ export class ListPageComponent {
       numMembers: 'N человек',
     }
   ]
+
+  constructor(private renderer: Renderer2) {}
+  currentSlideIndex = 0;
+  ngAfterViewInit() {
+    this.checkDeadEnd();
+  }
   navigateCarousel(direction: string) {
     const carousel = document.querySelector('.carousel') as HTMLElement | null;
     if (carousel) {
       if (direction === 'prev') {
         const prevButton = carousel.querySelector('.carousel-control-prev') as HTMLElement | null;
         prevButton?.click();
+        this.currentSlideIndex--;
+        this.checkDeadEnd();
       } else if (direction === 'next') {
         const nextButton = carousel.querySelector('.carousel-control-next') as HTMLElement | null;
         nextButton?.click();
+        this.currentSlideIndex++;
+        this.checkDeadEnd();
       }
+    }
+  }
+  checkDeadEnd() {
+    let totalSlides;
+    if (this.cards.length <= 3) {
+      totalSlides = 1;
+    } else {
+      totalSlides = this.cards.length-2;
+    }
+    const leftCursor = document.querySelector('.prev') as HTMLElement | null;
+    const rightCursor = document.querySelector('.next') as HTMLElement | null;
+    const slide = document.querySelector('.slide') as HTMLElement | null;
+    if (this.currentSlideIndex === 0 && leftCursor && slide) {
+      leftCursor.style.display = 'none';
+      this.renderer.setStyle(slide, 'margin-left', '238px');
+    } else if (leftCursor) {
+      leftCursor.style.display = 'block';
+      this.renderer.removeStyle(slide, 'margin-left');
+    }
+    if (this.currentSlideIndex === totalSlides - 1 && rightCursor && slide) {
+      rightCursor.style.display = 'none';
+      this.renderer.setStyle(slide, 'margin-right', '238px');
+    } else if (rightCursor) {
+      rightCursor.style.display = 'block';
+      this.renderer.removeStyle(slide, 'margin-right');
     }
   }
 }
