67d10f1d120ea186ac79147cba747b2b38fe5de0
darleet <darleet@mail.ru>
2023-09-19 02:22:01 +0300

    Fixed imports

--- micro_services/ApiGateWay/web_api_rpc/web_api_pb2.pyi
+++ micro_services/ApiGateWay/web_api_rpc/web_api_pb2.pyi
@@ -1,5 +1,5 @@
-import base_types_pb2 as _base_types_pb2
-import bot_pb2 as _bot_pb2
+import micro_services.ApiGateWay.my_types.base_types_pb2 as _base_types_pb2
+import micro_services.ApiGateWay.clients.bot.bot_pb2 as _bot_pb2
 from google.protobuf import descriptor as _descriptor
 from typing import ClassVar as _ClassVar
 
