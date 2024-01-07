8fbad1876f95be4cfc8e9b2ba0f67d3766e9638f
darleet <darleet@mail.ru>
2024-01-07 21:24:29 +0300

    ITSR-123 Implemented gRPC server

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -3,5 +3,6 @@ package errors
 import "errors"
 
 var ErrBotNotFound = errors.New("bot with provided id was not found")
+var ErrBotConflict = errors.New("bot with provided id already exists")
 var ErrUserNotFound = errors.New("user with provided id was not found")
 var ErrQuestionNotFound = errors.New("question with provided id was not found")
--- internal/telegram/delivery/grpc.go
+++ internal/telegram/delivery/grpc.go
@@ -1,6 +1,53 @@
 package delivery
 
+import (
+	domain "Registration-Bot/internal/domain/errors"
+	"Registration-Bot/internal/proto"
+	"context"
+	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
 type Service interface {
 	StartBot(botID int, token string) error
 	StopBot(botID int) error
+}
+
+// Server is an implementation of proto.BotRunnerServer
+type Server struct {
+	service Service
+	proto.UnimplementedBotRunnerServer
+}
+
+func NewServer(s Service) proto.BotRunnerServer {
+	return &Server{
+		service: s,
+	}
+}
+
+// StartBot accepts botID, token and log level in request and starts a bot with
+// provided parameters.
+func (s *Server) StartBot(_ context.Context, req *proto.StartBotRequest) (*emptypb.Empty, error) {
+	err := s.service.StartBot(int(req.GetId()), req.GetToken())
+	if err != nil && errors.Is(err, domain.ErrBotConflict) {
+		return nil, status.Errorf(codes.AlreadyExists, err.Error())
+	}
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &emptypb.Empty{}, nil
+}
+
+// StopBot accepts botID in request and stops the bot with provided ID if it exists.
+func (s *Server) StopBot(_ context.Context, req *proto.StopBotRequest) (*emptypb.Empty, error) {
+	err := s.service.StopBot(int(req.GetId()))
+	if err != nil && errors.Is(err, domain.ErrBotNotFound) {
+		return nil, status.Errorf(codes.NotFound, err.Error())
+	}
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	return &emptypb.Empty{}, nil
 }
