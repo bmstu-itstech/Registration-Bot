be293939beda2a3f7c867d4daeaff66d9ccbd448
darleet <darleet@mail.ru>
2024-01-08 01:43:37 +0300

    ITSR-105 Fixed types in grpc adapter func

--- internal/telegram/repository/grpc/client.go
+++ internal/telegram/repository/grpc/client.go
@@ -13,8 +13,12 @@ func NewAdapter(cl proto.BotGatewayClient) *Client {
 	return &Client{cl: cl}
 }
 
-func (a *Client) PushAnswers(answers map[int32]string) error {
-	_, err := a.cl.SaveAnswers(context.Background(), &proto.AnswersMap{Answers: answers})
+func (a *Client) PushAnswers(answers map[int]string) error {
+	reqData := make(map[int32]*proto.Answer)
+	for k, v := range answers {
+		reqData[int32(k)] = &proto.Answer{Data: &proto.Answer_Text{Text: v}}
+	}
+	_, err := a.cl.SaveAnswers(context.Background(), &proto.AnswersMap{Answers: reqData})
 	if err != nil {
 		return err
 	}
