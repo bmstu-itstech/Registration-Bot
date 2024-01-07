ce2ceefc45708f8bb8311135afa13fa50121c45f
darleet <darleet@mail.ru>
2024-01-07 23:24:33 +0300

    ITSR-105 Implemented grpc+local repository

---
+++ internal/telegram/repository/client/grpc.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"Registration-Bot/internal/proto"
+	"context"
+)
+
+type Client struct {
+	cl proto.BotGatewayClient
+}
+
+func NewAdapter(cl proto.BotGatewayClient) *Client {
+	return &Client{cl: cl}
+}
+
+func (a *Client) PushAnswers(answers map[int]string) error {
+	_, err := a.cl.SaveAnswers(context.Background(), &proto.AnswersMap{Answers: answers})
+	if err != nil {
+		return err
+	}
+	return nil
+}
